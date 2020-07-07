4a4b6e81bd6909ceff392fc7fa97fa9c8949c4a0
Daniel Santiago <danielsantiago111@hotmail.com>
2020-07-07 10:42:14 -0300

    Adicionado hook pra encriptar a senha

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -6,6 +7,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -16,6 +18,14 @@ class User extends Model {
         updatedAt: 'updated_at',
       }
     );
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
   }
 }
 
