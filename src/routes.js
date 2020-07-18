ef418694f2be17326dadbefcad6555ee071d60b5
Daniel Santiago <danielsantiago111@hotmail.com>
2020-07-18 13:32:47 -0300

    sinceramente mudou tanta coisa que nem sei mais

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-
+import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+routes.put('/users', authMiddleware, UserController.update);
 
 export default routes;
